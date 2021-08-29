9a68213a7c0c07bb47f1b64a99a15883290fa172
imtiazpy <ahmed.imti1994@gmail.com>
2021-08-29 21:34:15 +0600

    completed cart functionality

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const searchBtn = document.getElementById('search-btn');
+const cartList = [];
 searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
     searchFood()
@@ -45,9 +46,8 @@ const showFood = (data) => {
                         <img src="${e.strMealThumb}" class="card-img-top p-2 img-fluid food-img" alt="...">
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">${e.strMeal}</h5>
-                        <p class="card-text">${e.strInstructions.slice(0, 100)}<p>
-                        <button onclick="loadDetail(${e.idMeal})" class="btn btn-outline-secondary" type="button" id="detail-btn">Detail</button>
+                        <h5 class="card-title text-center">${e.strMeal}</h5>
+                        <button onclick="loadDetail(${e.idMeal})" class="btn btn-outline-secondary w-100" type="button" id="detail-btn">Detail</button>
                     </div>
                 </div>  
             `
@@ -70,22 +70,26 @@ const showLoadedDetail = mealDetail => {
     window.scrollTo(0, 40);
     const meal = mealDetail.meals[0]
     console.log(meal)
-    const { strMeal, strInstructions, strMealThumb, strYoutube } = meal;
+    const { idMeal, strMeal, strInstructions, strMealThumb, strYoutube } = meal;
     const detailContainer = document.getElementById('detail-container');
     detailContainer.textContent = '';
 
     detailContainer.innerHTML = `
-        <div class="card mx-5 p-3 w-3/4">
-            <div class="w-50 px-5 mx-auto">
-                <img src="${strMealThumb}" class="card-img-top img-fluid food-img" alt="...">
-            </div>
+        <div class="card mx-5 p-3 w-3/4">   
             <div class="card-body">
-                <h5 class="card-title">${strMeal}</h5>
-                <h6>Ingredients:</h6>
-                <ul id="ingredient-list"></ul>
+                <div class="d-flex flex-row">
+                    <div>
+                        <h5 class="card-title">${strMeal}</h5>
+                        <h6>Ingredients:</h6>
+                        <ul id="ingredient-list"></ul>
+                    </div>
+                    <div class="w-50 mx-auto">
+                        <img src="${strMealThumb}" class="card-img-top img-fluid food-img" alt="...">
+                    </div>
+                </div>
                 <p class="card-text">${strInstructions}</p>
                 <a href="${strYoutube}" class="btn btn-primary" target="_blank">Checkout Video</a>
-                <a href="#" onclick="addToCart()" class="btn btn-primary">Add to Cart</a>
+                <a href="#" onclick="addToCart(${idMeal})" class="btn btn-primary">Add to Cart</a>
             </div>
         </div>
     `
@@ -112,7 +116,40 @@ const showLoadedDetail = mealDetail => {
 }
 
 
-const addToCart = () => {
+const addToCart = async (id) => {
 
+    const cartContainer = document.getElementById('cart-container');
+    cartContainer.textContent = '';
+    url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+
+    const res = await fetch(url);
+    const data = await res.json()
+    const meal = data.meals[0]
+
+    cartList.push(meal);
+
+    document.getElementById('cart-count').innerText = cartList.length;
+
+    for (item of cartList) {
+        const { strMeal, strMealThumb } = item;
+
+        const div = document.createElement('div');
+        div.classList.add('row', 'gy-5', 'mb-2');
+
+        div.innerHTML = `
+            <div class="col-md-6">
+                <img src="${strMealThumb}" class="img-fluid rounded-circle" alt="..." width="50">
+            </div>
+            <div class="col-md-6">
+                <h3>${strMeal}</h3>
+            </div>
+        `
+        cartContainer.appendChild(div);
+    }
 }
+
+
+// const showError = () => {
+
+// }
 
